4a06d2d85c9e912d493eb8560a51ca0ef45a0627
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-09-03 22:43:24 +0100

    add RandomFact tag to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import RandomFacts from './Containers/RandomFacts';
 
 function App() {
   return (
     <div className="App">
+      <RandomFacts/>
     </div>
   );
 }
