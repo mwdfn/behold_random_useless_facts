43ea95157ef798caae87996db30a7c11e056ffe4
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-09-03 22:41:22 +0100

    add and set up RandomFact.js file

--- src/Containers/RandomFacts.js
+++ src/Containers/RandomFacts.js
@@ -1,3 +1,14 @@
 import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
 
+const RandomFacts = () => {
 
+    return(
+        <>
+            <h1>I'm where a random fact will appear!</h1>
+        </>
+    )
+
+}
+
+export default RandomFacts
