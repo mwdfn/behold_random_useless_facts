3f47b71a5823419b433efd383b7938b1bc5fc5d0
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-09-03 23:00:35 +0100

    add getRandomFacts function to fetch api

--- src/Containers/RandomFacts.js
+++ src/Containers/RandomFacts.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, Link } from 'react-router-dom';
 
 const RandomFacts = () => {
+    const [randomFact, setRandomFact] = useState('')
+
+    useEffect(() => {
+        getRandomFacts()
+    }, [])
+    
+    const getRandomFacts = () => {
+        fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+        .then(res => res.json())
+        .then(randomFact => setRandomFact(randomFact))
+        .catch(err => console.error('No facts retrieved.'))
+    }
 
     return(
         <>
