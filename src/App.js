09607c7b55566fdb32d491ae0fdbe07bd0085c30
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-09-03 23:45:51 +0100

    add ThemeProvider and dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import RandomFacts from './Containers/RandomFacts';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
       <RandomFacts/>
-    </div>
+    </ThemeProvider>
   );
 }
 
