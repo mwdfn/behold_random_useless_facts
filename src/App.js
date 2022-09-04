f63e8d3f2e8e4694e04de49c55bff7d59bda2645
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-09-04 01:23:14 +0100

    refactor dark mode and remove component directory and DarkMode.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, createContext } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import RandomFacts from './Containers/RandomFacts';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CssBaseline from '@mui/material/CssBaseline';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 function App() {
 
-  const mainTheme = useTheme();
   const [mode, setMode] = useState('light');
   const colorMode = useMemo(
   () => ({
@@ -35,25 +34,12 @@ function App() {
 
   return (
     <>
-        <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={mainTheme}>
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      {theme.palette.mode} mode
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
+    {theme.palette.mode} mode
+    <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+      {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+    </IconButton>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <RandomFacts />
       </ThemeProvider>
